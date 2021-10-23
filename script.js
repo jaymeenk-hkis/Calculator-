bfc0a94e981b6712b158adbfa312552d34f577cf
jaymeenk-hkis <92735902+jaymeenk-hkis@users.noreply.github.com>
2021-10-23 14:36:38 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = document.querySelector("#btn");
+let number = document.querySelector("#number");
+
+a.addEventListener('click', function (){
+
+number.value = "7";
+});
+alert("jaymeen");
+ 
